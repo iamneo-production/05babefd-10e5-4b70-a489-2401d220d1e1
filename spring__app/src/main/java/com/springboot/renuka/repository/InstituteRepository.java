038231261fbc73bbbef7c779363a80762bb1a64e
RenukaVangala <111485977+RenukaVangala@users.noreply.github.com>
2022-10-02 01:08:05 +0530

    spring__app

---
+++ spring__app/src/main/java/com/springboot/renuka/repository/InstituteRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.renuka.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.renuka.model.InstituteModel;
+
+public interface InstituteRepository extends JpaRepository<InstituteModel, Integer> {
+
+}
+
+
