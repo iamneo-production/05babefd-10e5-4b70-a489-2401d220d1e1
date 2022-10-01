d7beff84a3839337fe1464648b199b0acd53fca7
RenukaVangala <111485977+RenukaVangala@users.noreply.github.com>
2022-10-02 00:38:44 +0530

    spring_app

---
+++ spring_app/src/main/java/com/springboot/renuka/RenukaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.renuka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenukaApplication.class, args);
+	}
+
+}
