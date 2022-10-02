273971deb6414b597848ef46bf4a1757b394c3c4
RenukaVangala <111485977+RenukaVangala@users.noreply.github.com>
2022-10-02 07:54:46 +0530

    SPRING--app

---
+++ SPRING--app/src/main/java/com/springboot/renuka/AbacusAcademyAdmissionPortalApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.renuka;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusAcademyAdmissionPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacusAcademyAdmissionPortalApplication.class, args);
+	}
+
+}
